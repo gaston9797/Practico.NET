12ab8eab834aa4e6e5e739c171333589b98a3520
gaston9797 <gaston_gonzalez_12@hotmail.com>
2019-09-06 17:49:24 -0300

    PruebaController agregado

---
+++ ApiRest/Models/Prueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiRest.Models
+{
+    public class Prueba
+    {
+        public int id;
+        public string nombre;
+    }
+}
