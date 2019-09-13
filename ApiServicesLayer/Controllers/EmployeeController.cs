56aa0d6e8817959e431146846d607730f002febe
gaston9797 <gaston_gonzalez_12@hotmail.com>
2019-09-13 17:31:11 -0300

    Api Rest funcionando

--- ApiServicesLayer/Controllers/EmployeeController.cs
+++ ApiServicesLayer/Controllers/EmployeeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLogicLayer;
+using DataAccessLayer;
+using Shared;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,31 +13,79 @@ namespace ApiServices.Controllers
 {
     public class EmployeeController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        private IBLEmployees _bl;
+
+        public EmployeeController()
         {
-            return new string[] { "value1", "value2" };
+            IDALEmployees dal = new DALEmployeesEF();
+            _bl = new BLEmployees(dal);
+        }
+        // GET api/<controller>
+        public IHttpActionResult Get()
+        {
+            var json = JsonConvert.SerializeObject(_bl.GetAllEmployees());
+            return Ok(json);
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            var json = JsonConvert.SerializeObject(_bl.GetEmployee(id));
+            return Ok(json);
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Shared.Entities.ApiEmployee emp)//Esto es cualquier cosa
         {
+            if (emp.Type == 1)
+            {
+                Shared.Entities.PartTimeEmployee e = new Shared.Entities.PartTimeEmployee();
+                e.Name = emp.Name;
+                e.StartDate = emp.StartDate;
+                e.HourlyRate = emp.Salary;
+                _bl.AddEmployee(e);
+            }
+            else {
+                if (emp.Type == 2) {
+                    Shared.Entities.FullTimeEmployee e = new Shared.Entities.FullTimeEmployee();
+                    e.Name = emp.Name;
+                    e.StartDate = emp.StartDate;
+                    e.Salary = emp.Salary;
+                    _bl.AddEmployee(e);
+                }
+            }   
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Shared.Entities.ApiEmployee emp)
         {
+            Shared.Entities.Employee empleado = _bl.GetEmployee(id);
+            if (empleado.GetType() == typeof(Shared.Entities.FullTimeEmployee))
+            {
+                Shared.Entities.FullTimeEmployee e = new Shared.Entities.FullTimeEmployee();
+                e.Id = empleado.Id;
+                e.Name = emp.Name;
+                e.StartDate = emp.StartDate;
+                e.Salary = emp.Salary;
+                _bl.UpdateEmployee(e);
+            }
+            else {
+                if (empleado.GetType() == typeof(Shared.Entities.PartTimeEmployee))
+                {
+                    Shared.Entities.PartTimeEmployee e = new Shared.Entities.PartTimeEmployee();
+                    e.Id = empleado.Id;
+                    e.Name = emp.Name;
+                    e.StartDate = emp.StartDate;
+                    e.HourlyRate = emp.Salary;
+                    _bl.UpdateEmployee(e);
+                }
+            }
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+            _bl.DeleteEmployee(id);
         }
     }
 }
